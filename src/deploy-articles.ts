a5994cc24aeffea9afc3ffd5eab9cb220b5ead79
Bujakowski Bartosz <bartosz.bujakowski@talentdigital.eu>
2024-07-03 09:40:53 +0200

    Add basic article image upload

--- src/deploy-articles.ts
+++ src/deploy-articles.ts
@@ -1,9 +1,12 @@
-const { readFile, readdir } = require("fs").promises;
-const yamlFront = require("yaml-front-matter");
+import { promises as fsPromises } from "fs";
+import * as yamlFront from "yaml-front-matter";
 import { join } from "path";
 import * as core from "@actions/core";
 import got from "got";
 import { ArticleWeb } from "@talentdigital/api-client";
+import fetch from "node-fetch";
+
+const { readFile, readdir } = fsPromises;
 
 type DeployArticlesInput = {
   authorization: string;
@@ -26,16 +29,7 @@ export const deployArticles = async ({
     const mdFiles = files.filter((file) => file.endsWith(".md"));
     core.info(`Found ${mdFiles.length} files`);
 
-    const serverArticles = (await got
-      .get(`${baseUrl}/api/v1/articles`, {
-        headers: {
-          authorization,
-        },
-        searchParams: {
-          drafts: true,
-        },
-      })
-      .json()) as ArticleWeb[];
+    const serverArticles = await getServerArticles(authorization, baseUrl);
 
     for (const file of mdFiles) {
       const content = await readFile(join(path, file), {
@@ -50,30 +44,116 @@ export const deployArticles = async ({
         continue;
       }
 
-      core.info(`Deploying article ${JSON.stringify(article)}\n`);
+      const imageName = article.teaser?.split("/").pop();
 
-      const json: ArticleWeb = {
-        author: article.author ?? "",
-        title: article.title ?? "",
-        created: article.created,
-        tags: article.tags ?? [],
-        teaser: article.teaser ?? "",
-        content: article.__content.trim() ?? "",
-        draft: article.draft ?? true,
-        videoFileName: article.videoFileName ?? "",
-        abstract: article.abstract ?? "",
-      };
-
-      await got.post(`${baseUrl}/api/v1/articles`, {
-        headers: {
-          authorization,
-        },
-        json,
-      });
+      await uploadImage(article, authorization, baseUrl, imageName, rootPath);
+      await uploadArticle(article, authorization, baseUrl, imageName);
     }
   } catch (e) {
     core.setFailed(JSON.stringify(e));
   }
 
   core.info("\nArticles deploy completed\n");
+};
+
+const uploadImage = async (
+  article: any, // TODO
+  authorization: string,
+  baseUrl: string,
+  fileName: string,
+  rootPath: string
+) => {
+  const blob: Blob = await getBlobFromFilePath(join(rootPath, article.teaser));
+
+  const imageFile = new File([blob], fileName, {
+    type: "image",
+  });
+
+  const formData = new FormData();
+
+  formData.append(
+    "files",
+    new File([imageFile as Blob], fileName, {
+      type: "image",
+    })
+  );
+
+  try {
+    const response = await fetch(`${baseUrl}/api/v1/assets/image/upload`, {
+      method: "POST",
+      headers: {
+        authorization,
+      },
+      body: formData,
+    });
+
+    if (!response.ok) {
+      const errorBody = await response.text();
+      throw new Error(
+        `Failed to upload: ${response.status} ${response.statusText} - ${errorBody}`
+      );
+    }
+  } catch (error) {
+    if (error.response) {
+      console.error("Error response from server:", error.response.body);
+    } else {
+      console.error("Request error:", error);
+    }
+    throw error;
+  }
+};
+
+const uploadArticle = async (
+  article: any, // TODO?
+  authorization: string,
+  baseUrl: string,
+  fileName: string
+) => {
+  core.info(`Deploying article ${JSON.stringify(article)}\n`);
+
+  const json: ArticleWeb = {
+    author: article.author ?? "",
+    title: article.title ?? "",
+    created: article.created,
+    tags: article.tags ?? [],
+    teaser: fileName ?? "",
+    content: article.__content.trim() ?? "",
+    draft: article.draft ?? true,
+    videoFileName: article.videoFileName ?? "",
+    abstract: article.abstract ?? "",
+  };
+
+  await got.post(`${baseUrl}/api/v1/articles`, {
+    headers: {
+      authorization,
+    },
+    json,
+  });
+};
+
+const getServerArticles = async (
+  authorization: string,
+  baseUrl: string
+): Promise<ArticleWeb[]> => {
+  return await got
+    .get(`${baseUrl}/api/v1/articles`, {
+      headers: {
+        authorization,
+      },
+      searchParams: {
+        drafts: true,
+      },
+    })
+    .json();
+};
+
+const getBlobFromFilePath = async (filePath) => {
+  try {
+    const absolutePath = join(filePath);
+    const fileBuffer = await readFile(absolutePath);
+    return new Blob([fileBuffer]);
+  } catch (error) {
+    console.error("Error reading file:", error);
+    throw error;
+  }
 };
